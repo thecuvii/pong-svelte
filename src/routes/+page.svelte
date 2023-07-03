01ad83de1d1a557f311372ac25c6d14d4ec0889e
Cuvii <xw5427@gmail.com>
2023-07-03 20:58:34 +0800

    chore: init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	let count = 0;
+	$: {
+		console.log(count * 2);
+	}
+</script>
+
+<button on:click={() => (count += 1)}>{count}</button>
---
+++ src/routes/+page.ts
@@ -0,0 +1,8 @@
+import type { Config } from '@sveltejs/adapter-vercel';
+// since there's no dynamic data here, we can prerender
+// it so that it gets served as a static asset in production
+export const prerender = true;
+
+export const config: Config = {
+	runtime: 'edge'
+};
